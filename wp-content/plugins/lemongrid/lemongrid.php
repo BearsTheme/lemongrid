36c0fee25d8f7b6b319e39d832d03791dba13bc2
Bearsthemes <bearsthemes@gmail.com>
2015-10-21 16:10:35 +0700

    lemongrid plugin

---
+++ wp-content/plugins/lemongrid/lemongrid.php
@@ -0,0 +1,61 @@
+<?php
+/**
+*
+* Plugin Name: Lemon Grid
+* Plugin URI: http://themebears.com
+* Description: This plugin is addon visual composer, which is developed by THEMEBEARS Team for Visual Comporser plugin.
+* Version: 1.0.0
+* Author: BEATS Theme
+* Author URI: http://themebears.com
+* Copyright 2015 themebears.com. All rights reserved.
+*/
+
+define( 'TB_NAME', 'bearstheme' );
+define( 'TB_DIR', plugin_dir_path(__FILE__) );
+define( 'TB_URL', plugin_dir_url(__FILE__) );
+define( 'TB_INCLUDES', TB_DIR . "includes" . DIRECTORY_SEPARATOR );
+
+define( 'TB_CSS', TB_URL . "assets/css/" );
+define( 'TB_JS', TB_URL . "assets/js/" );
+define( 'TB_IMAGES', TB_URL . "assets/images/" );
+
+/**
+ * Use LemonGrid class
+ */
+new LemonGrid();
+
+/**
+ * LemonGrid Class
+ * 
+ */
+class LemonGrid() 
+{
+	/**
+	 * Init function, which is run on site init and plugin loaded
+	 */
+	public function __construct()
+	{
+		/**
+		 * Enqueue Scripts on plugin
+		 */
+		add_action( 'wp_enqueue_scripts', array( $this, 'register_script' ) );
+	}
+
+	/**
+	 * Register script on plugin
+	 */
+	function register_script()
+	{	
+		/**
+		 * Lib JS Gridstack
+		 */
+		wp_register_script( 'gridstack', TB_JS . 'gridstack.js', array( 'jquery' ) );
+		wp_register_style('gridstack', TB_CSS . 'gridstack.css', array(), '1.0');
+
+		/**
+		 * Script LemonGrid
+		 */
+		wp_register_script( 'tb-lemongrid-script', TB_JS . 'lemongrid.js', array( 'jquery', 'gridstack' ) );
+		wp_register_style('tb-lemongrid-script', TB_CSS . 'lemongrid.css', array(), '1.0');
+	}
+}
