c897898e256a2478f02a3c82638026e9f5e00601
Bearsthemes <bearsthemes@gmail.com>
2015-10-23 17:15:29 +0700

    update

--- wp-content/plugins/lemongrid/templates/social_lemongrid.php
+++ wp-content/plugins/lemongrid/templates/social_lemongrid.php
@@ -8,8 +8,8 @@ $social = $attr['social'];
  * @param array $data
  * @return HTML
  */
-if( ! function_exists( 'instagramSocialTemp' ) ) :
-	function instagramSocialTemp( $datas )
+if( ! function_exists( 'itemSocialTemp' ) ) :
+	function itemSocialTemp( $datas, $social )
 	{
 		$output = '';
 		$grid = renderGridDefault( count( $datas ) );
@@ -19,10 +19,24 @@ if( ! function_exists( 'instagramSocialTemp' ) ) :
 				"background: url({$data['photo']}) no-repeat center center / cover, #FFF", 
 				) );
 
+			$info = '';
+			switch ( $social ) {
+				case 'flickr':
+					
+					break;
+				
+				default: /* instagram */
+					$info .= '
+					<div class=\'lemongrid-info\'>
+						<h2 class=\'lemongrid-title\'>'. esc_attr( $data['description'] ) .'</h2>
+					</div>';
+					break;
+			}
+
 			$output .= '
 				<div class=\'lemongrid-item grid-stack-item\' data-gs-x=\''. esc_attr( $grid[$k]['x'] ) .'\' data-gs-y=\''. esc_attr( $grid[$k]['y'] ) .'\' data-gs-width=\''. esc_attr( $grid[$k]['w'] ) .'\' data-gs-height=\''. esc_attr( $grid[$k]['h'] ) .'\'>
 					<div class=\'grid-stack-item-content\' style=\''. esc_attr( $style ) .'\'>
-						
+						'. __( $info ) .'
 					</div>
 				</div>';
 		endforeach;
@@ -31,25 +45,12 @@ if( ! function_exists( 'instagramSocialTemp' ) ) :
 	}
 endif;
 
-/**
- * flickrSocialTemp
- *
- * @param array $data
- * @return HTML
- */
-if( ! function_exists( 'flickrSocialTemp' ) ) :
-	function flickrSocialTemp( $datas )
-	{
-		return 'hello flickr';
-	}
-endif;
-
 ?>
 <div class="lemongrid-wrap lemongrid-social social-<?php esc_attr_e( $social ); ?>">
 	<div class="lemongrid-inner grid-stack">
 		<?php 
 		if( is_array( $attr['media'] ) && count( $attr['media'] ) > 0 ) :
-			_e( call_user_func( $social . 'SocialTemp', $attr['media'] ) );
+			_e( call_user_func( 'itemSocialTemp', $attr['media'], $social ) );
 		else :
 			/* error message */
 			_e( '...', TB_NAME );
