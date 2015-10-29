e39d7857560fd7ba908f9a13a0ad68c82ac3a753
Bearsthemes <bearsthemes@gmail.com>
2015-10-29 16:40:35 +0700

    update

--- wp-content/plugins/lemongrid/assets/js/lemongrid.js
+++ wp-content/plugins/lemongrid/assets/js/lemongrid.js
@@ -2,12 +2,74 @@
 	'use strict';
 
 	/**
+	 * Dynamics animates
+	 */
+	function lgAnimFaceIn( $elems, $opts ) {
+		/**
+		 * Check $elems exist
+		 */
+		if( $elems.length <= 0 ) 
+			return;
+
+		var _opts = $.extend( {
+			frequency	: 300,
+			friction 	: 435,
+			duration 	: 1000,
+			delay 		: 100,
+		}, $opts );
+
+		$elems.each( function( e ) {
+			var index = $( this ).index();
+
+			dynamics.css( this, { opacity: 0, translateY: 40 } )
+
+			dynamics.animate( this, {
+		      	opacity: 1, translateY: 0
+		    }, {
+		      	type 		: dynamics.spring, 
+		      	frequency	: _opts.frequency, 
+		      	friction 	: _opts.friction, 
+		      	duration 	: _opts.duration, 
+		      	delay 		: _opts.delay + index * 100
+		    } )
+		} )
+	}
+
+	function lgAnimtranslate( $elems, $opts ) {
+		/**
+		 * Check $elems exist
+		 */
+		if( $elems.length <= 0 ) 
+			return;
+
+		var _opts = $.extend( {
+			frequency	: 0,
+			friction 	: 243,
+			duration 	: 731,
+			delay 		: 100,
+		}, $opts );
+
+		$elems.each( function( e ) {
+			var index = $( this ).index();
+
+			dynamics.animate( this, {
+			    translateY: 0, opacity: 1,
+		  	}, {
+			    type 		: dynamics.spring, 
+			    duration 	: _opts.duration, 
+			    friction 	: _opts.friction, 
+			    delay 		: _opts.delay + index * 100
+		  	} )
+		} )
+	}
+
+	/**
 	 * Confirm
 	 */
 	$.lgConfirm = function( opts, callback ) {
 		var content = $( '<div>', {
 			class: 'lg-modal-wrap',
-			html: '<div class=\'lg-modal-inner\'><p class=\'lg-modal-title\'>'+ opts.title +'</p><div class=\'lg-modal-body\'>'+ opts.content +'</div><button class=\'lg-modal-ok\'>'+ opts.btnText +'</button></div>',
+			html: '<div class=\'lg-modal-inner\'><p class=\'lg-modal-title lg-dynamics-anim\'>'+ opts.title +'</p><div class=\'lg-modal-body lg-dynamics-anim\'>'+ opts.content +'</div><button class=\'lg-modal-ok lg-dynamics-anim\'>'+ opts.btnText +'</button></div>',
 		} );
 
 		/**
@@ -27,78 +89,81 @@
 		} )
 
 		$( 'body' ).append( content );
+
+		lgAnimFaceIn( content.find( '.lg-dynamics-anim' ) );
+
+		return content;
 	}
 	
 	/**
 	 * DynamicsModal
 	 */
 	$.lgDynamicsModalPhoto = function( opts, callback ) {
-		var content = $( '<div>', {
+		this.content = $( '<div>', {
 			class: 'lg-dynamics-modal-wrap',
 			html: '<a href=\'#\' class=\'lg-dynamics-modal-close\'><i class=\'ion-ios-close-empty\'></i></a><div class=\'lg-dynamics-modal-inner\'><div class=\'lg-dynamics-modal-image\'><img src=\''+ opts.photo +'\'/></div><div class=\'lg-dynamics-modal-detail\'>'+ opts.detail +'</div></div>',
 		} );
 
-		content.find( 'a.lg-dynamics-modal-close' ).on( 'click', function( e ) {
+		var self = this;
+		this.content.find( 'a.lg-dynamics-modal-close' ).on( 'click', function( e ) {
 			e.preventDefault();
-			content.remove();
+			self.close();
 		} )
 
-		$( 'body' ).append( content );
+		this.content.on( 'close', function() {
+			self.close();
+		} )
+
+		$( 'body' ).append( this.content );
 
 		/**
 		 * Animate
 		 */
-		var modalInner = content.find( '.lg-dynamics-modal-inner' ),
-			imgWrap = content.find( '.lg-dynamics-modal-image' ),
-			detailWrap = content.find( '.lg-dynamics-modal-detail' ),
-			closeBtn = content.find( '.lg-dynamics-modal-close' );
+		this.modalInner = this.content.find( '.lg-dynamics-modal-inner' );
+		this.imgWrap 	= this.content.find( '.lg-dynamics-modal-image' );
+		this.detailWrap 	= this.content.find( '.lg-dynamics-modal-detail' );
+		this.closeBtn 	= this.content.find( '.lg-dynamics-modal-close' );
 
-		dynamics.animate( content[0], {
+		dynamics.animate( this.content[0], {
 		    opacity: 1,
 	  	}, {
 		    type: dynamics.spring, duration: 1500, frequency: 1, friction: 250
 	  	} )
 
-	  	dynamics.animate( closeBtn[0], {
-		    translateY: 0,
-	  	}, {
-		    type: dynamics.spring, duration: 500, delay: 1000
-	  	} )
+		lgAnimtranslate( self.closeBtn, { duration: 500, delay: 1000 } );
 
 		setTimeout( function() {
-			dynamics.animate( imgWrap[0], {
-			    translateY: 0,
-			    opacity: 1,
-		  	}, {
-			    type: dynamics.spring, duration: 731, friction: 243
-		  	} )
+			lgAnimtranslate( self.imgWrap );
 		}, 200 )
 
-		if( detailWrap.find( '.lg-animate-fadein' ).length > 0 ) {
+		if( self.detailWrap.find( '.lg-animate-fadein' ).length > 0 ) {
 			setTimeout( function() {
-				detailWrap.find( '.lg-animate-fadein' ).each( function( e ) {
-					var index = $( this ).index();
-
-					dynamics.css( this, {
-				      	opacity: 0,
-				      	translateY: 30
-				    } )
-
-					dynamics.animate( this, {
-				      	opacity: 1,
-				      	translateY: 0
-				    }, {
-				      	type: dynamics.spring,
-				      	frequency: 300,
-				      	friction: 435,
-				      	duration: 1000,
-				      	delay: 100 + index * 100
-				    } )
-				} )
+				lgAnimFaceIn( self.detailWrap.find( '.lg-animate-fadein' ) );
 			}, 300 )
 		}
 
-		return content;
+		return this;
+	}
+
+	$.lgDynamicsModalPhoto.prototype.close = function() {
+		var self = this;
+		dynamics.animate( self.imgWrap[0], {
+		    translateY 	: 500,
+		    opacity 	: 0,
+	  	}, {
+		    type: dynamics.spring, duration: 1110, frequency: 619, friction: 660, anticipationSize: 268, anticipationStrength: 356
+	  	} )
+
+	  	setTimeout( function() {
+	  		dynamics.animate( self.content[0], {
+			    opacity: 0,
+		  	}, {
+			    type: dynamics.spring, duration: 1110, frequency: 619, friction: 660, anticipationSize: 268, anticipationStrength: 356,
+			    complete: function() {
+			    	self.content.remove();
+			    }
+		  	} )
+	  	}, 0 )
 	}
 
 	/**
@@ -126,7 +191,7 @@
 		return result;
 	}
 
-	function lgApplyLemonGrid( data ) {
+	function lgSaveLemonGrid( data ) {
 		console.log( data );
 	}
 
@@ -164,7 +229,7 @@
 			var lemonGridMap = getLemonGridSize( item.find( '.grid-stack > .grid-stack-item:visible' ) ),
 				grid_name = $( this ).data( 'grid-name' );
 
-			self._request( 'lgApplyLemonGrid', { name: grid_name, gridMap: lemonGridMap }, lgApplyLemonGrid );
+			self._request( 'lgApplyLemonGrid', { name: grid_name, gridMap: lemonGridMap }, lgSaveLemonGrid );
 		} )
 
 		/**
@@ -180,7 +245,7 @@
 				btnText: 'Add' }, 
 				function( content ) {
 					var grid_name = content.find( '#lg-grid-name' ).val();
-					self._request( 'lgApplyLemonGrid', { name: grid_name, gridMap: lemonGridMap }, lgApplyLemonGrid );
+					self._request( 'lgApplyLemonGrid', { name: grid_name, gridMap: lemonGridMap }, lgSaveLemonGrid );
 				} )
 		} );
 	}
@@ -230,6 +295,71 @@
 	}
 
 	/**
+	 * get next / prev 
+	 */
+	function lgGetSiblings( selector, index ) {
+		var prevElem = $( selector, 'body' ).eq( index - 1 ),
+			nextElem = $( selector, 'body' ).eq( index + 1 );
+
+		return { next: nextElem, prev: prevElem };
+	}
+
+	/**
+	 * next prev handle modal
+	 */
+	function handleNextPrevModal( selector, index, callback ) {
+
+		var content = $( '<div>', {
+				class: 'lg-next-prev-wrap',
+				html: '<a href=\'#\' class=\'lg-btn-prev\' title=\'prev\'><i class=\'ion-ios-arrow-thin-left\'></i></a><a href=\'#\' class=\'lg-btn-next\' title=\'next\'><i class=\'ion-ios-arrow-thin-right\'></i></a>',
+			} );
+
+		var prevEl = content.find( '.lg-btn-prev' ),
+			nextEl = content.find( '.lg-btn-next' );
+
+		prevEl.on( 'click', function( e ) {
+			e.preventDefault();
+
+			var siblings = lgGetSiblings( '[data-instagram]', index ),
+				$elem = siblings.prev;
+
+			callback.call( this, $elem, index - 1,'prev' );
+		} )
+
+		nextEl.on( 'click', function( e ) {
+			e.preventDefault();
+
+			var siblings = lgGetSiblings( '[data-instagram]', index ),
+				$elem = siblings.next;
+
+			callback.call( this, $elem, index + 1, 'next' );
+		} )
+
+		return content;
+	}
+
+	/**
+	 * Call modal Instagram
+	 */
+	function instagramCallModal( $elem, index, direct ) {
+		var data = $elem.data( 'instagram' ),
+			next_prev_wrap = handleNextPrevModal( '[data-instagram]', index, instagramCallModal );
+
+		if( $( '.lg-dynamics-modal-wrap' ).length > 0 ) {
+			$( '.lg-dynamics-modal-wrap' ).trigger( 'close' );
+		}
+
+		var modal = new $.lgDynamicsModalPhoto( {
+			photo: data.photo,
+			detail: data.detail_modal,
+		} );
+
+		modal.content.find( '.lg-dynamics-modal-detail' ).prepend( next_prev_wrap );
+
+		return modal;
+	}
+
+	/**
 	 * DOM load complete
 	 */
 	$( function() {
@@ -240,19 +370,15 @@
 		new lemonGrid( '.lemongrid--element' );
 
 		/**
-		 * Social Modal Detail
+		 * Social Modal Instagram
 		 */
-		$( '.lemongrid-wrap' ).on( 'click', '[data-instagram]', function( e ) {
+		$( 'body' ).on( 'click', '[data-instagram]', function( e ) {
 			e.preventDefault();
 			var $thisEl = $( this ),
-				data = $thisEl.data( 'instagram' );
+				index = $( '[data-instagram]', 'body' ).index( this );
 
-			var modal = $.lgDynamicsModalPhoto( {
-				photo: data.photo,
-				detail: data.detail_modal,
-			} );
-
-
+			var modal = instagramCallModal( $thisEl, index );
 		} )
+
 	} )
 } )( jQuery )
